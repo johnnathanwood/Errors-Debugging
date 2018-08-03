db8f784006c0c224bebba58e28e02940d2b406ce
John Wood <johnnathanwood@gmail.com>
2018-08-03 15:54:48 -0500

    practice debugger with meg

--- main.js
+++ main.js
@@ -9,19 +9,28 @@ button.addEventListener("click", function(){
 
 //print the junior instructors to the DOM
 let jrInstructors = document.getElementById("jr-instructors")
-staff.forEach(jrInstructor => {
-    jrInstructors.appendChild(`${jrInstructor} is a junior instructor at NSS`);
+staff.juniorInstructors.forEach(jrInstructor => {
+    let element = document.createElement("p")
+    element.textContent = `${jrInstructor} is a junior instructor at NSS`
+    jrInstructors.appendChild(element);
 });
 
 
 //console log each of the schools in the school array individually and console log them
-schools.forEach(schools => {
-    console.log(schools);
-});
+
+// schools.forEach(schools => {
+//     console.log(schools);
+// });
+
+for(let i = 0; i < schools.length; i++) {
+    console.log(schools[i]);
+};
+
+
 
 
 //print each of the lead instructors to the console
-for(let i = 0; i <= staff.instructors.length; i++){
+for(let i = 0; i < staff.instructors.length; i++){
     console.log(staff.instructors[i]);
 }
 
@@ -29,7 +38,8 @@ for(let i = 0; i <= staff.instructors.length; i++){
 //print the result to the console
 let parkInfo = {};
 fetch("https://external-data-56670.firebaseio.com/park-info.json")
+.then(response => response.json())
 .then(result => {
-    parkInfo = result;
-});
+parkInfo = result;
 console.log(parkInfo);
+});
